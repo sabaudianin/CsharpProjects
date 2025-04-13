048baac20dabbccf60062fb55f8167dbfd94416c
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 <sabaudianin@gmail.com>
2025-04-13 19:27:20 +0200

    C# challanges

---
+++ arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿string[] orders=new string[3];
+
+orders[0]="Order 1";
+orders[1]="Order 2";
+orders[2]="Order 3";
+
+
+orders[1]="New Order 2";
+Console.WriteLine(orders[1]);
+
+string[] idOrder=["1","2","3"];
+string[] someOrder={"1","2","3"};
+Console.WriteLine(idOrder[1]);
+
+Console.WriteLine($"array length:{idOrder.Length}");
+
+
+string[] names={"John","Jane","Jack"};
+Console.WriteLine($"Array Names length:{names.Length}");
+
+foreach(string name in names){
+    Console.WriteLine(name);
+}
+
+int [] inventory={100,200,300,400,500};
+
+int bin=0;
+int sum=0;
+foreach(int item in inventory){
+    Console.WriteLine(item);  
+    sum+=item;  
+    bin++;
+    Console.WriteLine($"Bin {bin} has {item} items and temp {sum}");    
+}
+
+
+Console.WriteLine($"Total inventory: {sum}");
