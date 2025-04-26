06754c0da1c6ad7999db5669ec09a4f8648a6c32
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 <sabaudianin@gmail.com>
2025-04-26 20:00:56 +0200

    Methods with params module finished

---
+++ Methods/params/Program.cs
@@ -0,0 +1,195 @@
+﻿// int[] schedule = { 800, 1200, 1600, 2000 };
+
+// DisplayAdjustedTime(schedule,6,-6);
+
+// void DisplayAdjustedTime(int[] times, int currentGMT, int newGMT)
+// {
+//     int diff = 0;
+//     if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+//     {
+//         Console.WriteLine("Invalid GMT");
+//     }
+//     else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+//     {
+//         diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+//     }
+//     else
+//     {
+//         diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+//     }
+
+//     for (int i = 0; i<times.Length; i++)
+//     {
+//         int newTime = ((times[i] + diff)) % 2400;
+//         Console.WriteLine($"{times[i]} -> {newTime}");
+//     }
+// }
+
+// string [] students ={"Jenna","Aisza","Carlos","Viktor"};
+
+// DisplayStudents(students);
+// DisplayStudents(new string[]{"Stacy","Kelly"});
+
+// void DisplayStudents(string[] students)
+// {
+//     foreach(string student in students)
+//     {
+//         Console.WriteLine(student);
+//     }
+//     Console.WriteLine();
+// }
+
+
+// double pi = 3.14159;
+// PrintCircleInfo(12);
+// PrintCircleInfo(24);
+
+// void PrintCircleInfo(int radius) 
+// {
+//     Console.WriteLine($"Circle with radius {radius}");
+//     PrintCircleArea(radius);
+//     PrintCircleCircumference(radius);
+// }
+
+// void PrintCircleArea(int radius)
+// {
+//     double area = pi * (radius * radius);
+//     Console.WriteLine($"Area = {area}");
+// }
+
+// void PrintCircleCircumference(int radius)
+// {
+//     double circumference = 2 * pi * radius;
+//     Console.WriteLine($"Circumference = {circumference}");
+// }
+
+// int a = 3;
+// int b = 4;
+// int c = 0;
+
+// Multiply(a,b,c);
+// Console.WriteLine($"global :{a} x {b} = {c}");
+
+// void Multiply(int a,int b, int c)
+// {
+//     c = a * b;
+//     Console.WriteLine($"inside: {a} x {b} = {c}");
+
+// }
+
+// int [] array ={1,2,3,4};
+
+// PrintArray(array);
+// Clear(array);
+// PrintArray(array);
+
+// void PrintArray(int[] array)
+// {
+//     foreach(int i in array)
+//     {
+//         Console.Write($"{i}");
+//     }
+//     Console.WriteLine();
+// }
+
+// void Clear (int[] array)
+// {
+//     for(int i =0; i<array.Length;i++)
+//     {
+//         array[i] = 0;
+//     }
+// }
+
+
+// string status = "Healthy";
+
+// Console.WriteLine($"Start: {status}");
+// SetHealth(false);
+// Console.WriteLine($"End: {status}");
+
+// void SetHealth(bool isHealthy)
+// {
+//     status=(isHealthy ? "Healthy":"Unhealthy");
+//     Console.WriteLine($"Middle : {status}");
+// }
+
+
+// string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+// string[] rsvps = new string[10];
+// int count = 0;
+
+// RSVP("Rebecca");
+// RSVP("Nadia", 2, "Nuts");
+// RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+// RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+// RSVP("Noor", 4, inviteOnly: false);
+// RSVP("Jonte", 2, "Stone fruit", false);
+// ShowRSVPs();
+
+
+// void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+// {
+//     if (inviteOnly)
+//     {
+//         bool found = false;
+//         foreach (string guest in guestList)
+//         {
+//             if (guest.Equals(name))
+//             {
+//                 found = true;
+//                 break;
+//             }
+//         }
+//         if (!found)
+//         {
+//             Console.WriteLine($"Sorry, {name} is not on the guest list");
+//             return;
+//         }
+//     }
+
+//     rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+//     count++;
+// }
+
+// void ShowRSVPs()
+// {
+//     Console.WriteLine("\n Total RSVPs:");
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.WriteLine(rsvps[i]);
+//     }
+// }
+
+
+
+string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+   DisplayEmail(first:corporate[i,0],last:corporate[i,1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    DisplayEmail(first:external[i,0],last:external[i,1],domain:externalDomain);
+}
+
+void DisplayEmail(string first, string last, string domain = "contoso.com")
+{
+    string email = first.Substring(0,2) + last;
+    email = email.ToLower();
+    Console.WriteLine($"{email}@{domain}");
+}
